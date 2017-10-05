a2a267c54344c41778b3c6802e11eefa06a5a0cf
avjarami <alex.v.jaramillo@intel.com>
2017-10-05 10:06:55 -0700

    Adding 3day population chart

--- telemetryui/telemetryui/__init__.py
+++ telemetryui/telemetryui/__init__.py
@@ -19,14 +19,18 @@ from flask import (
     request,
     url_for)
 from . import config
-from .jinja_filters import timesince, local_datetime_since, basename
+from .jinja_filters import (
+    timesince,
+    local_datetime_since,
+    basename,
+    )
 import importlib
 
 app = Flask(__name__, static_folder="static", static_url_path="/telemetryui/static")
 app.config.from_object(config.Config)
 
 try:
-   #try importing from the local dev configuration if it exists
+   # try importing from the local dev configuration if it exists
    from . import config_local
    app.config.from_object(config_local.Config)
 except:
