f12e1ff831fc4365f2203045c44cad81dea54709
Alex Jaramillo <alex.v.jaramillo@intel.com>
2018-10-08 18:35:52 +0000

    Adding uWSGI dependencies
    Spool processing of crashes requires uwsgidecorators module, in clear
    linux the module uwsgi is provided by a bundle but not the decorator.
    This change adds installation of uwsgidecorators module to section where
    requirements are installed based on the distro.
    
    Signed-off-by: Alex Jaramillo <alex.v.jaramillo@intel.com>

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -246,9 +246,9 @@ _install_pip_pkgs() {
   sudo rm -f "$log"
   if [ $DISTRO == "clr" ]; then
     # the latest psycopg2 binary package is incompatible with the glibc 2.26 on Clear Linux
-    sudo bash -c "https_proxy=$https_proxy source venv/bin/activate && https_proxy=$https_proxy pip3 --log $log install --no-binary psycopg2 -r $reqs"
+    sudo bash -c "https_proxy=$https_proxy source venv/bin/activate && https_proxy=$https_proxy pip3 --log $log install --no-binary psycopg2 -r $reqs uwsgidecorators"
   else
-    sudo bash -c "https_proxy=$https_proxy source venv/bin/activate && https_proxy=$https_proxy pip3 --log $log install -r $reqs"
+    sudo bash -c "https_proxy=$https_proxy source venv/bin/activate && https_proxy=$https_proxy pip3 --log $log install -r $reqs uwsgi uwsgidecorators"
   fi
 }
 
