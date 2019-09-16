ca7e080b713b4d74267df473e6e05cbd5d6eb65e
Alex Jaramillo <alex.v.jaramillo@intel.com>
2019-09-16 09:45:01 -0700

    Enable autoreload on change in dev environment
    entrypoint.sh : add argument to autoreload application when module is
                    changed in development environment.
    
    Signed-off-by: Alex Jaramillo <alex.v.jaramillo@intel.com>

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -4,4 +4,5 @@ export FLASK_APP=/var/www/telemetry/telemetryui/run.py
 pushd /var/www/telemetry/telemetryui/
 
 flask db upgrade
-uwsgi --http 0.0.0.0:5000 --chdir /var/www/telemetry/telemetryui/ -w telemetryui:app --spooler /var/www/telemetry/telemetryui/uwsgi-spool
+uwsgi --http 0.0.0.0:5000 --chdir /var/www/telemetry/telemetryui/ -w telemetryui:app \
+      --spooler /var/www/telemetry/telemetryui/uwsgi-spool --py-autoreload 1
